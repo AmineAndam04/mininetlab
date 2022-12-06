1e9c00f4d6d3ba7ad591886dfd6f23736f5ba24a
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-12-06 14:45:41 +0100

    Add files via upload

---
+++ Fig2-Topo.py
@@ -0,0 +1,20 @@
+from mininet.topo import Topo,node,link
+
+class MyTopo( Topo,node,link ):
+
+    def build( self ):
+        
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        Switch = self.addSwitch( 's1' )
+
+        # Add links
+        self.addLink( leftHost , Switch )
+        self.addLink( rightSwitch, Switch )
+        self.setCPUFrac(leftHost,0.2)
+        self.TCIntf.config(rightHost,100,50)
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
