283ec726cc1b3afeeb6c1e96650a52a804b16afb
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-12-06 13:26:49 -0800

    Update Fig1-Topo.py

--- Fig1-Topo.py
+++ Fig1-Topo.py
@@ -1,18 +1,16 @@
 
-
 from mininet.topo import Topo
-
 class MyTopo( Topo ):
 
     def build( self ):
         
 
         # Add hosts and switches
-        leftHost1 = self.addHost( 'h3' )
-        leftHost2 = self.addHost( 'h4' )
-        rightHost = self.addHost( 'h5' )
-        leftSwitch = self.addSwitch( 's1' )
-        rightSwitch = self.addSwitch( 's2' )
+        leftHost1 = self.addHost( 'h3' ,ip="10.0.1.2/24")
+        leftHost2 = self.addHost( 'h4', ip="10.0.1.3/24" )
+        rightHost = self.addHost( 'h5', ip="10.0.2.2/24" )
+        leftSwitch = self.addSwitch( 's1',ip="10.0.1.1/24" )
+        rightSwitch = self.addSwitch( 's2',ip="10.0.2.1/24" )
 
         # Add links
         self.addLink( leftHost1 , leftSwitch )
