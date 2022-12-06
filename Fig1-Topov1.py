a39f9f4efda70e60c1de2ab1c27dd0bd349c82d8
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-12-06 14:01:25 -0800

    Add files via upload

---
+++ Fig1-Topov1.py
@@ -0,0 +1,25 @@
+from mininet.topo import Topo
+class MyTopo( Topo ):
+
+    def build( self ):
+        
+
+        # Add hosts and switches
+        leftHost1 = self.addHost( 'h3' ,ip="10.0.1.2/24")
+        leftHost2 = self.addHost( 'h4', ip="10.0.1.3/24" )
+        rightHost = self.addHost( 'h5', ip="10.0.2.2/24" )
+        leftSwitch = self.addSwitch( 's1' )
+        rightSwitch = self.addSwitch( 's2')
+
+        # Add links
+        self.addLink( leftHost1 , leftSwitch )
+        self.addLink( leftHost2 , leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+        
+        leftSwitch.setIP("10.0.1.1")
+        
+        rightSwitch.setIP("10.0.2.1")
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
