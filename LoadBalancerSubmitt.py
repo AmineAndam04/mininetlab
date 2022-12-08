3c8b936d90156ad1f0bd4158cbb2c3f90f98c67a
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-12-08 07:29:29 -0800

    Add files via upload

---
+++ LoadBalancerSubmitt.py
@@ -0,0 +1,103 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.util import dpidToStr
+import pox.lib.packet as pkt
+
+log = core.getLogger()
+s1_dpid=0
+s2_dpid=0
+s3_dpid=0
+s4_dpid=0
+s5_dpid=0
+
+def _handle_ConnectionUp (event):
+    
+      global s1_dpid, s2_dpid, s3_dpid, s4_dpid, s5_dpid
+
+      for m in event.connection.features.ports:
+            
+            #Switch 1
+            if m.name == "s1-eth1":
+
+                  
+                  #flood ARP packets
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x806),action=of.ofp_action_output(port=of.OFPP_ALL))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(priority=of.OFP_DEFAULT_PRIORITY +1,match=of.ofp_match(dl_type=0x0800,nw_proto=6,nw_src="10.0.0.2",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=4))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.1",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=3))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.1"),action=of.ofp_action_output(port=1))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.2"),action=of.ofp_action_output(port=2))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.2",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=5))
+                  event.connection.send(msg)
+
+            elif m.name == "s2-eth1":
+
+                  #flood ARP packets
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x806),action=of.ofp_action_output(port=of.OFPP_ALL))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.1",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=2))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.1"),action=of.ofp_action_output(port=1))
+                  event.connection.send(msg)
+
+            elif m.name == "s3-eth1":
+
+                  #flood ARP packets
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x806),action=of.ofp_action_output(port=of.OFPP_ALL))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.2",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=2))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.2"),action=of.ofp_action_output(port=1))
+                  event.connection.send(msg)
+
+            elif m.name == "s4-eth1":
+
+                  #flood ARP packets
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x806),action=of.ofp_action_output(port=of.OFPP_ALL))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.2",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=2))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.2"),action=of.ofp_action_output(port=1))
+                  event.connection.send(msg)
+
+            elif m.name == "s5-eth1":
+                  
+                  #flood ARP packets
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x806),action=of.ofp_action_output(port=of.OFPP_ALL))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.1",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=4))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.2",nw_dst="10.0.0.3"),action=of.ofp_action_output(port=4))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.1"),action=of.ofp_action_output(port=1))
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(priority=of.OFP_DEFAULT_PRIORITY +1,match=of.ofp_match(dl_type=0x800,nw_proto=6,nw_src="10.0.0.3",nw_dst="10.0.0.2"),action=of.ofp_action_output(port=2))
+                  
+                  event.connection.send(msg)
+
+                  msg = of.ofp_flow_mod(match=of.ofp_match(dl_type=0x800,nw_src="10.0.0.3",nw_dst="10.0.0.2"),action=of.ofp_action_output(port=3))
+                  event.connection.send(msg)
+
+
+
+def launch ():
+  core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
